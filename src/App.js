7d97260cdccc5d2aa9b6fbc62e6b09c521781f84
bowersjuan <bowersjuan@gmail.com>
2022-11-01 22:54:13 -0400

    Install Browser Router packcage and update paths for home, stafflist and petslist

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 /*
   Components
@@ -25,11 +26,22 @@ function App() {
 
   return (
     <div className="wrapper">
-      <Nav />
-      <Home employees={employees} owners={owners} pets={pets} />
-      <StaffList employees={employees} />
-      <PetsList pets={pets} />
-      <Footer />
+      <Router>
+        <Nav />
+        <Routes>
+          <Route
+            path="/"
+            element={<Home employees={employees} owners={owners} pets={pets} />}
+          />
+          <Route
+            path="/stafflist"
+            element={<StaffList employees={employees} />}
+          />
+
+          <Route path="/petslist" element={<PetsList pets={pets} />} />
+        </Routes>
+        <Footer />
+      </Router>
     </div>
   );
 }
